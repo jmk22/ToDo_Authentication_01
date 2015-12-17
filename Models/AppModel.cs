3999758fc4b0ec14bf6799b3982fa095d5164606
jill <jill@epicodus.com>
2015-12-17 10:43:18 -0800

    Add to do list views with authentication

---
+++ Models/AppModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace AuthenticationApp03.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual ICollection<ToDo> ToDoes { get; set; }
+    }
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("name=DefaultConnection")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<ToDo> ToDoes { get; set; }
+    }
+}
